b2780951657b238f8980575f9e9df87ffef31ac0
hamide.yuce <hamide.yuce@keyyazilim.com>
2020-07-17 13:18:57 +0300

    first commit

---
+++ web-jobs-exe/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace web_jobs_exe
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
